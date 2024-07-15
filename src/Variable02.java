a5960b97f6e68251230beeef643d283cd139eb73
RedCatHuu <148026462+RedCatHuu@users.noreply.github.com>
2024-07-15 15:22:15 +0900

    変数02

---
+++ src/Variable02.java
@@ -0,0 +1,17 @@
+
+public class Variable02 {
+	
+	static final int MIN_NUMBER = 10;
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println(MIN_NUMBER);
+		// MIN_NUMBER = 20;
+		
+		final int maxValue;
+		maxValue = 100;
+		System.out.println(maxValue);
+		
+		
+	}
+
+}
